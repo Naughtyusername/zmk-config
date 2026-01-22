81c88359d0d6493257e18225bcb41acc122b42b2
naughtyarch <52771412+Naughtyusername@users.noreply.github.com>
2026-01-22 00:55:56 -0800

    ugh

--- config/layouts.h
+++ config/layouts.h
@@ -1,11 +1,44 @@
-/* This represents a standard 3-row, 10-column + 6-thumb layout (36 keys) */
-// clang-format off
-#define LAYOUT_36_CORE( \
-    K00, K01, K02, K03, K04,    K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14,    K15, K16, K17, K18, K19, \
-    K20, K21, K22, K23, K24,    K25, K26, K27, K28, K29, \
-              K30, K31, K32,    K33, K34, K35 \
-)
+/* clang-format off */
+/* clang-format off */
+#pragma once
+
+// --- SOFLE MAPPING ---
+#if defined(SHIELD_SOFLE_LEFT) || defined(SHIELD_SOFLE_RIGHT)
+
+    #define LAYOUT_36( \
+        k00, k01, k02, k03, k04,    k05, k06, k07, k08, k09, \
+        k10, k11, k12, k13, k14,    k15, k16, k17, k18, k19, \
+        k20, k21, k22, k23, k24,    k25, k26, k27, k28, k29, \
+                  k30, k31, k32,    k33, k34, k35 \
+    ) \
+    &none &none &none &none &none &none                        &none &none &none &none &none &none \
+    &none k00   k01   k02   k03   k04                              k05   k06   k07   k08   k09   &none \
+    &none k10   k11   k12   k13   k14                              k15   k16   k17   k18   k19   &none \
+    &none k20   k21   k22   k23   k24   &none                &none k25   k26   k27   k28   k29   &none \
+                      k30   k31   k32   &none                &none k33   k34   k35
+
+// --- CRADIO / SWEEP MAPPING ---
+#elif defined(SHIELD_CRADIO_LEFT) || defined(SHIELD_CRADIO_RIGHT) || defined(SHIELD_SWEEP)
+
+    #define LAYOUT_36( \
+        k00, k01, k02, k03, k04,    k05, k06, k07, k08, k09, \
+        k10, k11, k12, k13, k14,    k15, k16, k17, k18, k19, \
+        k20, k21, k22, k23, k24,    k25, k26, k27, k28, k29, \
+                  k30, k31, k32,    k33, k34, k35 \
+    ) \
+    k00 k01 k02 k03 k04    k05 k06 k07 k08 k09 \
+    k10 k11 k12 k13 k14    k15 k16 k17 k18 k19 \
+    k20 k21 k22 k23 k24    k25 k26 k27 k28 k29 \
+            k30 k31 k32    k33 k34 k35
+
+#endif
+
+// --- UROB-STYLE WRAPPER ---
+// This macro works for all boards because it just wraps whatever LAYOUT_36 produces
+#define ZMK_LAYER(name, layout) \
+    name { \
+        bindings = <layout>; \
+    };
 
 /*                                      60 KEY MATRIX / LAYOUT MAPPING
                                         SOFLE
@@ -29,3 +62,39 @@
   │ 20  21  22  23  24 │ 25  26  27  28  29 │ │ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 │
   ╰───────────╮ 30  31 │ 32  33 ╭───────────╯ ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯
               ╰────────┴────────╯                         ╰─────────┴─────────╯             */
+
+
+
+/******************************************************************************************************/
+/* /\* In your Sofle-specific logic *\/                                                               */
+/* #define ZMK_LAYER(name, layout)                                                \                   */
+/*     name {                                                                     \                   */
+/*         bindings = <&none & none & none & none & none & none & none & none &   \                   */
+/*                     none & none & none & none & none layout & none & none &    \                   */
+/*                     none & none & none & none & none & none & none & none &    \                   */
+/*                     none & none & none & none & none>;                         \                   */
+/*     };                                                                                             */
+/*                                                                                                    */
+/*                                                                                                    */
+/*                                                                                                    */
+/* /\* In your Cradio-specific logic (no padding needed) *\/                                          */
+/* #define ZMK_LAYER(name, layout) \                                                                  */
+/*     name { \                                                                                       */
+/*         bindings = <layout>; \                                                                     */
+/*     };                                                                                             */
+/*                                                                                                    */
+/* / {                                                                                                */
+/*     keymap {                                                                                       */
+/*         compatible = "zmk,keymap";                                                                 */
+/*                                                                                                    */
+/*         ZMK_LAYER(default,                                                                         */
+/*             LAYOUT_36_CORE(                                                                        */
+/*                 &kp Q, &kp W, &kp E, &kp R, &kp T,    &kp Y, &kp U, &kp I, &kp O, &kp P,           */
+/*                 &kp A, &kp S, &kp D, &kp F, &kp G,    &kp H, &kp J, &kp K, &kp L, &kp SEMI,        */
+/*                 &kp Z, &kp X, &kp C, &kp V, &kp B,    &kp N, &kp M, &kp COMMA, &kp DOT, &kp SLASH, */
+/*                               &kp LGUI, &mo 1, &kp SPC, &kp RET, &mo 2, &kp RALT                   */
+/*             )                                                                                      */
+/*         )                                                                                          */
+/*     };                                                                                             */
+/* };                                                                                                 */
+/******************************************************************************************************/
