9ac617394ccd8dbdbea86c6432c27e7e3c2b7f53
naughtyarch <52771412+Naughtyusername@users.noreply.github.com>
2026-01-23 19:04:22 -0800

    testing this

---
+++ config/corne_wrapper.h
@@ -0,0 +1,8 @@
+// config/corne_wrapper.h
+#define MY_KEYMAP(k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, k12,  \
+                  k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, k24,  \
+                  k25, k26, k27, k28, k29, k30, t1, t2, t3, t4, t5, t6)        \
+    &none, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, &none, &none,     \
+        k11, k12, k13, k14, k15, k16, k17, k18, k19, k20, &none, &none, k21,   \
+        k22, k23, k24, k25, k26, k27, k28, k29, k30, &none, t1, t2, t3, t4,    \
+        t5, t6
